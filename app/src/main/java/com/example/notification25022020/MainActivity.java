353d66111a5e2d5b9f32aa1fbd99d2a5c3e242d8
luongthanhdat0707 <luongthanhdat0707>
2020-05-05 20:28:04 +0700

    start

--- app/src/main/java/com/example/notification25022020/MainActivity.java
+++ app/src/main/java/com/example/notification25022020/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.notification25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnTao,mBtnDong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnTao = findViewById(R.id.buttonTaoNotification);
+        mBtnDong= findViewById(R.id.buttonDongNotification);
+        //1: Giao dien notification : NotificationCompat.Builder
+        // 2 : PendingIntent: Sau khi xử lý xong muốn kết nói với intent để xử lý cho việc gì
+        // 3 : NotificationManager : Để quản lý tất cả notification
+        
+        mBtnTao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNotification();
+            }
+        });
+    }
+
+    private void createNotification() {
     }
 }
